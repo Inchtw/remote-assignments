b0464247249fe6bc8891cb7fef6a21c79b73abef
Inchtw <ashin1207@gmail.com>
2020-05-08 17:39:53 +0800

    assignment4 upload

--- Week-4/Assignment-4/Advanced.js
+++ Week-4/Assignment-4/Advanced.js
@@ -1,4 +1,24 @@
+
+
+function delayedResultPromise(n1, n2, delayTime) {
+    return new Promise(function(resolve, reject){
+        setTimeout(()=>{
+            resolve(n1+n2);
+        },delayTime)
+    })
+    }
+    delayedResultPromise(4, 5 , 3000).then(console.log) // 9 (4+5) will be shown in the console after 3 seconds
+
+
+// function 用await 抓promise 一定要宣告為 async function
+// async/await 是方便簡寫promise的方式
+
 async function main() {
     // your code here, you should call delayedResultPromise here and get the result using async/await.
+    let result = await delayedResultPromise(-5, 10 , 2000);
+    console.log(result);
     }
     main() // result will be shown in the console after <delayTime> seconds
+
+
+    
